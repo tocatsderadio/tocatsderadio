6921142c65cbb6863ae1dc0b91cadc3ba6f969c9
tocatsderadio <92156161+tocatsderadio@users.noreply.github.com>
2021-10-11 13:48:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ var preLoad = function(){
   console.log("Installing web app");
   return caches.open("offline").then(function(cache) {
     console.log("caching index and important routes");
-    return cache.addAll(["/index.html"]);
+    return cache.addAll(["index.html"]);
   });
 };
 
@@ -42,7 +42,7 @@ var returnFromCache = function(request){
   return caches.open("offline").then(function (cache) {
     return cache.match(request).then(function (matching) {
      if(!matching || matching.status == 404) {
-       return cache.match("offline.html");
+       return cache.match("index.html");
      } else {
        return matching;
      }
